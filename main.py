f50159dc4af9d16bc86446094e306b780ca8adb4
Abdennour Ben Taieb <abdelnourbentaieb@gmail.com>
2026-01-04 16:58:33 +0100

    my initial commit : convert office files to pdf using libreoffice

---
+++ main.py
@@ -0,0 +1,31 @@
+import subprocess
+from pathlib import Path
+import os
+
+
+downloads = Path(r"D:\Downloads")
+libreoffice_path = r"C:\Program Files\LibreOffice\program\soffice.exe"
+newest_file = max(downloads.glob('*'), key=os.path.getctime)
+
+office_extensions = ['.docx', '.doc', '.xlsx', '.xls', '.pptx', '.ppt']
+if newest_file.suffix.lower() not in office_extensions:
+    print(f"Latest file is not an Office document: {newest_file.name}")
+    exit()
+
+
+
+print(f"Converting: {newest_file.name}")
+
+r= subprocess.run([
+    libreoffice_path,
+    '--headless',
+    '--convert-to', 'pdf',
+    '--outdir', str(downloads),
+    str(newest_file)
+] , capture_output=True , text=True)
+
+if r.returncode == 0:
+    pdf_name = newest_file.stem + '.pdf'
+    print(f"✓ Converted successfully: {pdf_name}")
+else:
+    print(f"✗ Conversion failed: {result.stderr}")
